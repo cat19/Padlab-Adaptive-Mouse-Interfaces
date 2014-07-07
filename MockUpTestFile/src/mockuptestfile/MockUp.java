1bc939bc175490b128765ce5da295e3dbac06674
cat19 <cat19@umbc.edu>
2014-07-07 14:24:26 -0400

    Added whitespace

--- MockUpTestFile/src/mockuptestfile/MockUp.java
+++ MockUpTestFile/src/mockuptestfile/MockUp.java
@@ -9,6 +9,7 @@ public class MockUp {
 
 	public static void main(String[] args) {
 		//System.out.println(returning(14.299,4673.484,177.34957,6578.2435798));
+		
 		System.out.println(returning(Double.parseDouble(args[0]),Double.parseDouble(args[1]),Double.parseDouble(args[2]),Double.parseDouble(args[3])));
 	}
 
